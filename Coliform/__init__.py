169c1264d8f355fa2d370bf0529a1400b2a9ceb8
Osvaldo E Duran <osvaldo.duran@upr.edu>
2016-09-25 16:04:59 -0400

    Changed License

--- Coliform/__init__.py
+++ Coliform/__init__.py
@@ -5,7 +5,7 @@
 # This file is part of Coliform. https://github.com/Regendor/coliform-project
 # (C) 2016
 #
-# Licensed under the Open Software License version 3.0 
+# Licensed under the GNU General Public License version 3.0 (GPL-3.0)
 import serial
 import os
 import glob
