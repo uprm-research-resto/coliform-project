1c5783e1e59fde94db9d72a77c4ac4d1d3cbe45d
Osvaldo E Duran <osvaldo.duran@upr.edu>
2016-09-25 16:14:08 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, Distribution
+
+
+class BinaryDistribution(Distribution):
+    def is_pure(self):
+        return False
+
+
+setup(
+    name='Coliform',
+    version='0.2.2',
+    description='Coliform UPRM Project Library, Written for Raspberry Pi',
+    packages=['Coliform'],
+    author='Osvaldo E Duran',
+    author_email='osvaldo.duran@upr.edu',
+    url='https://github.com/Regendor/coliform-project',
+    classifiers=['Programming Language :: Python :: 3','Intended Audience :: Education','Intended Audience :: Science/Research','License :: OSI Approved :: GNU General Public License v3 (GPLv3)'],
+    install_requires=['matplotlib','Pillow','pyserial'],
+    distclass=BinaryDistribution
+)
