10b9323c3fd1464fff826c6a1e5d1db36aba5d74
Regendor <osvaldo.duran09@gmail.com>
2017-04-04 21:08:01 -0400

    Update to 0.7.4.2

--- Coliform/RPiCameraBackend.py
+++ Coliform/RPiCameraBackend.py
@@ -19,11 +19,14 @@ class PiCamera(object):
         self.shutterspeed = 0  # microseconds
         self.exposure_mode = ''
         self.awb_mode = ''
-        self.timeout = 5000  # milliseconds
+        self.timeout = 5000  # milliseconds, The program will run for this length of time, then take the capture (if output is specified).
+        self.timelapse = 0 # milliseconds, The specific value is the time between shots in milliseconds. Note you should specify %04d at the point in the filename where you want a frame count number to appear. Example: image%04d.jpg
         self.zoom = (0.0, 0.0, 1.0, 1.0)
-        self.resolution = (2952, 1944)
+        self.resolution = (2952, 1944) # image resolution (width, height)
+        self.quality = 75 # jpeg compression indicator, where quality 100 is almost completely uncompressed
+        self.preview = 'n' # preview settings: n = no preview, f = full screen preview. You can also choose to display preview at a specific location on the screen. This is done by typing 'x,y,w,h' where x and y are the location on screen and w and h are the height and width of the image.
 
-    def capture(self, mode='JPG', filename='output.jpg'):
+    def capture(self, mode='JPG', filename='output.jpg'): # Note: remember, if you have a non zero timelapse, you have include %04d in filename. Example: image%04d.jpg
         if self.iso:
             iso = ' --ISO ' + str(self.iso)
         else:
@@ -54,10 +57,19 @@ class PiCamera(object):
         else:
             awb = ''
 
-        if self.timeout != 5:
+        if self.timeout != 5000:
             timeout = ' --timeout ' + str(self.timeout)
         else:
             timeout = ''
+
+        if self.timelapse != 0:
+            timelapse = ' --timelapse ' + str(self.timelapse)
+            if '%04d' in filename:
+                pass
+            else:
+                filename = 'image%04d.jpg'
+        else:
+            timelapse = ''
 
         if self.zoom != (0.0, 0.0, 1.0, 1.0):
             zoom = ' --roi ' + str(self.zoom).replace('(', '').replace(')', '').replace(' ', '')
@@ -69,26 +81,38 @@ class PiCamera(object):
         else:
             resolution = ''
 
+        if self.quality != 75:
+            quality = ' --quality ' + str(self.quality)
+        else:
+            quality = ''
+
+        if self.preview == 'n':
+            preview = ' --nopreview'
+        elif self.preview == 'f':
+            preview = ''
+        else:
+            preview = '-p ' + str(self.preview)
+
         if mode in ['PNG', 'png']:
             encode = ' --encoding png'
-            if filename == 'output.jpg':
-                filename = 'output.png'
+            if '.jpg' in filename:
+                filename = filename.replace('.jpg', '.png')
 
         elif mode in ['BMP', 'bmp']:
             encode = ' --encoding bmp'
-            if filename == 'output.jpg':
-                filename = 'output.bmp'
+            if '.jpg' in filename:
+                filename = filename.replace('.jpg', '.bmp')
 
         elif mode in ['GIF', 'gif']:
             encode = ' --encoding gif'
-            if filename == 'output.jpg':
-                filename = 'output.gif'
+            if '.jpg' in filename:
+                filename = filename.replace('.jpg', '.gif')
         else:
             encode = ''
 
-        os.system('raspistill --raw{}{}{}{}{}{}{}{}{}{}{}{}{}'.format(' --nopreview', iso, brightness, contrast,
-                                                                      shutterspeed, exposure, awb, timeout, zoom,
-                                                                      resolution, encode, ' -o ', filename))
+        os.system('raspistill --raw{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}'.format(preview, iso, brightness, contrast,
+                                                                      shutterspeed, exposure, awb, timeout, timelapse, zoom,
+                                                                      resolution, encode, quality, ' -o ', filename))
         if mode in ['RGB', 'rgb']:
             rgb_array = misc.imread(filename)
             return rgb_array
