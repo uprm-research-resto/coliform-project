1cc5d6752bdfee385563e6c45f779408851bbb1e
Regendor <osvaldo.duran09@gmail.com>
2017-04-04 21:20:18 -0400

    Update to 0.7.4.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name='Coliform',
-    version='0.7.4.1',
+    version='0.7.4.2',
     description='Coliform UPRM Project Library, Written for Raspberry Pi',
     packages=['Coliform'],
     author='Osvaldo E Duran',
